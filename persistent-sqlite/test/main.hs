882dc40eb1e85e15a5494914c59a7b117f0440f6
Boris Lykah <lykahb@gmail.com>
2021-04-26 11:46:07 -0600

    Restore tests

--- persistent-sqlite/test/main.hs
+++ persistent-sqlite/test/main.hs
@@ -18,35 +18,35 @@
 import SqliteInit
 
 import qualified CompositeTest
+import qualified CustomPersistFieldTest
+import qualified CustomPrimaryKeyReferenceTest
+import qualified DataTypeTest
+import qualified EmptyEntityTest
+import qualified EmbedOrderTest
+import qualified EmbedTest
+import qualified EquivalentTypeTest
+import qualified ForeignKey
+import qualified HtmlTest
+import qualified LargeNumberTest
+import qualified MaxLenTest
+import qualified MpsNoPrefixTest
+import qualified MpsCustomPrefixTest
+import qualified MigrationColumnLengthTest
+import qualified MigrationOnlyTest
+import qualified PersistentTest
+import qualified GeneratedColumnTestSQL
+import qualified PersistUniqueTest
+import qualified PrimaryTest
+import qualified RawSqlTest
+import qualified ReadWriteTest
+import qualified Recursive
+import qualified RenameTest
+import qualified SumTypeTest
+import qualified TransactionLevelTest
+import qualified UniqueTest
+import qualified UpsertTest
+import qualified LongIdentifierTest
 import qualified DerivePersistTest
--- import qualified CustomPersistFieldTest
--- import qualified CustomPrimaryKeyReferenceTest
--- import qualified DataTypeTest
--- import qualified EmptyEntityTest
--- import qualified EmbedOrderTest
--- import qualified EmbedTest
--- import qualified EquivalentTypeTest
--- import qualified ForeignKey
--- import qualified HtmlTest
--- import qualified LargeNumberTest
--- import qualified MaxLenTest
--- import qualified MpsNoPrefixTest
--- import qualified MpsCustomPrefixTest
--- import qualified MigrationColumnLengthTest
--- import qualified MigrationOnlyTest
--- import qualified PersistentTest
--- import qualified GeneratedColumnTestSQL
--- import qualified PersistUniqueTest
--- import qualified PrimaryTest
--- import qualified RawSqlTest
--- import qualified ReadWriteTest
--- import qualified Recursive
--- import qualified RenameTest
--- import qualified SumTypeTest
--- import qualified TransactionLevelTest
--- import qualified UniqueTest
--- import qualified UpsertTest
--- import qualified LongIdentifierTest
 
 import Control.Exception (handle, IOException, throwIO)
 import Control.Monad.Catch (catch)
@@ -69,9 +69,9 @@ import System.IO.Temp (withSystemTempFile)
 
 import Database.Persist.Sqlite
 import qualified Database.Sqlite as Sqlite
--- import PersistentTestModels
+import PersistentTestModels
 
--- import qualified MigrationTest
+import qualified MigrationTest
 
 type Tuple = (,)
 
@@ -182,25 +182,91 @@ main = do
 
   runConn $ do
     mapM_ setup
-      [ CompositeTest.compositeMigrate
-      , DerivePersistTest.derivePersistMigrate
-    --   , MigrationTest.migrationMigrate
-    --   , PersistUniqueTest.migration
-    --   , RenameTest.migration
-    --   , CustomPersistFieldTest.customFieldMigrate
-    --   , PrimaryTest.migration
-    --   , CustomPrimaryKeyReferenceTest.migration
-    --   , MigrationColumnLengthTest.migration
-    --   , TransactionLevelTest.migration
-    --   , LongIdentifierTest.migration
+      [ ForeignKey.compositeMigrate
+      , PersistentTest.testMigrate
+      , PersistentTest.noPrefixMigrate
+      , PersistentTest.customPrefixMigrate
+      , EmbedTest.embedMigrate
+      , EmbedOrderTest.embedOrderMigrate
+      , LargeNumberTest.numberMigrate
+      , UniqueTest.uniqueMigrate
+      , MaxLenTest.maxlenMigrate
+      , Recursive.recursiveMigrate
+      , CompositeTest.compositeMigrate
+      , MigrationTest.migrationMigrate
+      , PersistUniqueTest.migration
+      , RenameTest.migration
+      , CustomPersistFieldTest.customFieldMigrate
+      , PrimaryTest.migration
+      , CustomPrimaryKeyReferenceTest.migration
+      , MigrationColumnLengthTest.migration
+      , TransactionLevelTest.migration
+      , LongIdentifierTest.migration
+      , DerivePersistTest.migration
       ]
-    -- PersistentTest.cleanDB
-    -- ForeignKey.cleanDB
+    PersistentTest.cleanDB
+    ForeignKey.cleanDB
 
   hspec $ do
-    CompositeTest.specsWith db
-    DerivePersistTest.specsWith db
+    RenameTest.specsWith db
+    DataTypeTest.specsWith
+        db
+        (Just (runMigrationSilent dataTypeMigrate))
+        [ TestFn "text" dataTypeTableText
+        , TestFn "textMaxLen" dataTypeTableTextMaxLen
+        , TestFn "bytes" dataTypeTableBytes
+        , TestFn "bytesTextTuple" dataTypeTableBytesTextTuple
+        , TestFn "bytesMaxLen" dataTypeTableBytesMaxLen
+        , TestFn "int" dataTypeTableInt
+        , TestFn "intList" dataTypeTableIntList
+        , TestFn "intMap" dataTypeTableIntMap
+        , TestFn "bool" dataTypeTableBool
+        , TestFn "day" dataTypeTableDay
+        , TestFn "time" (DataTypeTest.roundTime . dataTypeTableTime)
+        , TestFn "utc" (DataTypeTest.roundUTCTime . dataTypeTableUtc)
+        ]
+        [ ("pico", dataTypeTablePico) ]
+        dataTypeTableDouble
+    HtmlTest.specsWith
+        db
+        (Just (runMigrationSilent HtmlTest.htmlMigrate))
+    EmbedTest.specsWith db
+    EmbedOrderTest.specsWith db
+    LargeNumberTest.specsWith db
+    UniqueTest.specsWith db
+    MaxLenTest.specsWith db
+    Recursive.specsWith db
+    SumTypeTest.specsWith db (Just (runMigrationSilent SumTypeTest.sumTypeMigrate))
+    MigrationOnlyTest.specsWith db
+        (Just
+            $ runMigrationSilent MigrationOnlyTest.migrateAll1
+            >> runMigrationSilent MigrationOnlyTest.migrateAll2
+        )
+    PersistentTest.specsWith db
+    PersistentTest.filterOrSpecs db
+    ReadWriteTest.specsWith db
+    RawSqlTest.specsWith db
+    UpsertTest.specsWith
+        db
+        UpsertTest.Don'tUpdateNull
+        UpsertTest.UpsertPreserveOldKey
 
+    MpsNoPrefixTest.specsWith db
+    MpsCustomPrefixTest.specsWith db
+    EmptyEntityTest.specsWith db (Just (runMigrationSilent EmptyEntityTest.migration))
+    CompositeTest.specsWith db
+    PersistUniqueTest.specsWith db
+    PrimaryTest.specsWith db
+    CustomPersistFieldTest.specsWith db
+    CustomPrimaryKeyReferenceTest.specsWith db
+    MigrationColumnLengthTest.specsWith db
+    EquivalentTypeTest.specsWith db
+    ForeignKey.specsWith db
+    TransactionLevelTest.specsWith db
+    MigrationTest.specsWith db
+    LongIdentifierTest.specsWith db
+    GeneratedColumnTestSQL.specsWith db
+    DerivePersistTest.specsWith db
 
     it "issue #328" $ asIO $ runSqliteInfo (mkSqliteConnectionInfo ":memory:") $ do
         void $ runMigrationSilent migrateAll
@@ -259,14 +325,14 @@ main = do
             void $ runMigrationSilent compositeMigrateTest
             validateForeignKeys
 
-    -- it "afterException" $ asIO $ runSqliteInfo (mkSqliteConnectionInfo ":memory:") $ do
-    --     void $ runMigrationSilent testMigrate
-    --     let catcher :: forall m. Monad m => SomeException -> m ()
-    --         catcher _ = return ()
-    --     insert_ $ Person "A" 0 Nothing
-    --     insert_ (Person "A" 1 Nothing) `catch` catcher
-    --     insert_ $ Person "B" 0 Nothing
-    --     return ()
+    it "afterException" $ asIO $ runSqliteInfo (mkSqliteConnectionInfo ":memory:") $ do
+        void $ runMigrationSilent testMigrate
+        let catcher :: forall m. Monad m => SomeException -> m ()
+            catcher _ = return ()
+        insert_ $ Person "A" 0 Nothing
+        insert_ (Person "A" 1 Nothing) `catch` catcher
+        insert_ $ Person "B" 0 Nothing
+        return ()
 
 validateForeignKeys
     :: (MonadResource m, MonadReader env m, BackendCompatible SqlBackend env)
--- persistent-test/src/DerivePersistTest.hs
+++ persistent-test/src/DerivePersistTest.hs
@@ -9,37 +9,37 @@ import Data.Maybe (isJust)
 import Init
 import Debug.Trace
 
-data Plain = Plain {
-  plainName :: String,
-  plainAge :: Int
+data DerivePrimary = DerivePrimary {
+  derivePrimaryName :: String,
+  derivePrimaryAge :: Int
 } deriving (Eq, Show)
 
 derivePersist persistSettings {mpsGeneric=False, mpsRecordFieldToHaskellName = stripEntityNamePrefix} lowerCaseSettings [
-  (mkDeriveEntityDef 'Plain) {
-    deriveFields = [(mkDeriveFieldDef 'plainName) {
+  (mkDeriveEntityDef 'DerivePrimary) {
+    deriveFields = [(mkDeriveFieldDef 'derivePrimaryName) {
       sqlNameOverride=Just "sql_name"
       }]
   }]
 
-data PlainPrimary = PlainPrimary {
-  plainPrimaryName :: String,
-  plainPrimaryRef :: PlainId
+data DeriveSimpleRef = DeriveSimpleRef {
+  plainPrimaryRefName :: String,
+  plainPrimaryRefRef :: DerivePrimaryId
 } deriving (Eq, Show)
 derivePersist persistSettings {mpsGeneric=False, mpsRecordFieldToHaskellName = stripEntityNamePrefix} lowerCaseSettings [
-  mkDeriveEntityDef 'PlainPrimary
+  mkDeriveEntityDef 'DeriveSimpleRef
   ]
 
-newtype SubType = SubType {
-  menuObject :: [MenuObject]
+newtype SubType2 = SubType2 {
+  menuObject :: [MenuObject2]
 } deriving (Eq, Show)
 
-newtype MenuObject = MenuObject {
-  sub :: Maybe SubType
+newtype MenuObject2 = MenuObject2 {
+  sub :: Maybe SubType2
 } deriving (Eq, Show)
 
 derivePersist persistSettings {mpsGeneric=False, mpsRecordFieldToHaskellName = stripEntityNamePrefix} lowerCaseSettings [
-  mkDeriveEntityDef 'SubType,
-  mkDeriveEntityDef 'MenuObject
+  mkDeriveEntityDef 'SubType2,
+  mkDeriveEntityDef 'MenuObject2
   ]
 
 data TestParent2 = TestParent2 {
@@ -65,55 +65,57 @@ derivePersist persistSettings {mpsGeneric=False, mpsRecordFieldToHaskellName = s
     }
   ]
 
-derivePersistMigrate = do
-  migrate [] (entityDef (Proxy :: Proxy Plain))
-  migrate [] (entityDef (Proxy :: Proxy PlainPrimary))
-  migrate [] (entityDef (Proxy :: Proxy SubType))
-  migrate [] (entityDef (Proxy :: Proxy MenuObject))
+migration :: Migration
+migration = do
+  migrate [] (entityDef (Proxy :: Proxy DerivePrimary))
+  migrate [] (entityDef (Proxy :: Proxy DeriveSimpleRef))
+  migrate [] (entityDef (Proxy :: Proxy SubType2))
+  migrate [] (entityDef (Proxy :: Proxy MenuObject2))
   migrate [] (entityDef (Proxy :: Proxy TestParent2))
   migrate [] (entityDef (Proxy :: Proxy TestChild2))
 
 specsWith :: (MonadIO m, MonadFail m) => RunDb SqlBackend m -> Spec
-specsWith runDb = describe "derivePersist" $
-  describe "primary keys" $ do
-    let p1 = Plain "a" 1
-    it "Supports basic operations" $ runDb $ do
-      k <- insert p1
-      p1' <- get k
-      Just p1 @== p1'
-    
-    it "Creates fields" $ runDb $ do
-      k <- insert p1
-      p1' <- selectFirst [PlainName ==. "a"] []
-      Just (Entity k p1) @== p1'
+specsWith runDb = describe "derivePersist" $ do
+  it "Supports basic operations" $ runDb $ do
+    let p1 = DerivePrimary "a" 1
+    k <- insert p1
+    p1' <- get k
+    Just p1 @== p1'
+  
+  it "Creates fields" $ runDb $ do
+    let p1 = DerivePrimary "a" 1
+    k <- insert p1
+    p1' <- selectFirst [DerivePrimaryName ==. "a"] []
+    Just (Entity k p1) @== p1'
 
-    it "Supports primary key" $ runDb $ do
-      k <- insert p1
-      let p = PlainPrimary "a" k
-      k' <- insert p
-      p' <- get k'
-      Just p @== p'
+  it "Supports primary key" $ runDb $ do
+    let p1 = DerivePrimary "a" 1
+    k <- insert p1
+    let p = DeriveSimpleRef "a" k
+    k' <- insert p
+    p' <- get k'
+    Just p @== p'
 
-    it "Supports recursive" $ runDb $ do
-      let m1 = MenuObject $ Just $ SubType []
-      let m2 = MenuObject $ Just $ SubType [m1]
-      let m3 = MenuObject $ Just $ SubType [m2]
-      k3 <- insert m3
-      m3' <- get k3
-      m3' @== Just m3
-      res <- rawSql "SELECT * FROM menu_object" []
-      liftIO $ print (res :: [(Single Text, Single Text)])
-    
-    it "Supports composite keys" $ runDb $ do
-      -- copy from CompositeTest
-      let p1 = TestParent2 "a1" "b1" 11 "p1"
-      let p2 = TestParent2 "a2" "b2" 22 "p2"
-      let c1 = TestChild2 "a1" "b1" 11 "c1"
-      kp1 <- insert p1
-      insert_ p2
-      kc1 <- insert c1
-      mc <- get kc1
-      isJust mc @== True
-      let Just c11 = mc
-      c1 @== c11
-      testChild2Fkparent c11 @== kp1
+  it "Supports recursive" $ runDb $ do
+    let m1 = MenuObject2 $ Just $ SubType2 []
+    let m2 = MenuObject2 $ Just $ SubType2 [m1]
+    let m3 = MenuObject2 $ Just $ SubType2 [m2]
+    k3 <- insert m3
+    m3' <- get k3
+    m3' @== Just m3
+    res <- rawSql "SELECT * FROM menu_object" []
+    liftIO $ print (res :: [(Single Text, Single Text)])
+  
+  it "Supports composite keys" $ runDb $ do
+    -- copy from CompositeTest
+    let p1 = TestParent2 "a1" "b1" 11 "p1"
+    let p2 = TestParent2 "a2" "b2" 22 "p2"
+    let c1 = TestChild2 "a1" "b1" 11 "c1"
+    kp1 <- insert p1
+    insert_ p2
+    kc1 <- insert c1
+    mc <- get kc1
+    isJust mc @== True
+    let Just c11 = mc
+    c1 @== c11
+    testChild2Fkparent c11 @== kp1
